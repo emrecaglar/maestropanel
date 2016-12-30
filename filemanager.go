e3aeba9655c3aa1953e26fecb55fc377f17c889b
Emre ÇAĞLAR <emre52@gmail.com>
2016-12-30 20:26:30 +0300

    add wercker

--- filemanager.go
+++ filemanager.go
@@ -1,10 +1,39 @@
 package maestropanel
 
 import (
-    _"encoding/json"
+    "encoding/json"
 )
 
 type FileManager struct {
     mp MaestroPanel
 }
 
+func (m *FileManager) SetWriteAccess(netRuntime NETRuntimeVersion)(result DomainExecutionResult, err error) {
+	result = DomainExecutionResult{}
+
+	response, err := m.mp.writeData(changeNETRuntimeVersionAction.Method, m.mp.getURL(changeNETRuntimeVersionAction), netRuntime)
+
+	if err == nil {
+		json.Unmarshal(response, &result)
+	}
+
+	return
+}
+
+func (m *Web) GetDotNetRuntimeVersion(domainName string)(result NETRuntimeResult, err error) {
+	result = NETRuntimeResult{}
+
+	extra := struct {
+		Name string `json:"name"`
+	} {
+		domainName,
+	}
+
+	response, err := m.mp.writeData(getNETRuntimeVersionAction.Method, m.mp.getURL(getNETRuntimeVersionAction), extra)
+
+	if err == nil {
+		json.Unmarshal(response, &result)
+	}
+
+	return
+}
