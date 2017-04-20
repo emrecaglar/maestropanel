16f3c087abdb4f73ca791f58889c7dd1d65a71c2
Emre ÇAĞLAR <emre52@gmail.com>
2017-04-20 21:42:30 +0300

    -

--- doc.go
+++ doc.go
@@ -1,7 +1,39 @@
 /*
 Package maestroPanel is a MaestroPanel API Client library
 
-Install Package
+For Example:
+package main
 
+import (
+	"fmt"
+	"github.com/emrecaglar/maestropanel"
+)
+
+func main(){
+	m := maestropanel.MaestroPanel{
+            Url:        "http://domain.com:9715",
+            Key:        "xxxxxx",
+            Version:    "v1"
+    }
+
+    domain := maestropanel.Domain{
+        Name:       "domain.com",
+        UserName:   "admin",
+        Password:   "111111",
+        PlanAlias:  "default",
+    }
+
+    web := m.Web()
+
+    result, _ := web.CreateDomain(domain)
+
+    fmt.Println(result)
+}
+
+MastroPanel'in aldığı parametreler
+Url: MaestroPanel API servisinin adres ve port bilgisi
+Key: MaestroPanel üzerinde oluşturulan API anahtarı
+Version: API versiyonu. Eğer versiyon belirtilmezse varsayılan olarak v1 kullanılacaktır
 */
+
 package maestropanel
